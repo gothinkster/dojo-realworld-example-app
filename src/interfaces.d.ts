dc3f469aa26841887730ca3469dc6a33ffe6d06b
Anthony Gubler <agubler@sitepen.com>
2020-03-11 23:43:39 +0000

    work in progress

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,4 +1,6 @@
 export type WithTarget<T extends Event = Event, E extends HTMLElement = HTMLInputElement> = T & { target: E };
+import { Session } from '../interfaces';
+import { OutletContext } from '@dojo/framework/routing/interfaces';
 
 export interface ResourceBased {
 	isLoaded: boolean;
@@ -113,4 +115,81 @@ interface State {
 	login: ResourceBased;
 	register: ResourceBased;
 	editor: Editor;
+}
+
+export interface SlugPayload {
+	slug: string;
+}
+
+export interface TitlePayload {
+	title: string;
+}
+
+export interface DescriptionPayload {
+	description: string;
+}
+
+export interface BodyPayload {
+	body: string;
+}
+
+export interface TagPayload {
+	tag: string;
+}
+
+export interface BioPayload {
+	bio: string;
+}
+
+export interface ImagePayload {
+	imageUrl: string;
+}
+
+export interface EmailPayload {
+	email: string;
+}
+
+export interface UsernamePayload {
+	username: string;
+}
+
+export interface PasswordPayload {
+	password: string;
+}
+
+export interface FollowUserPayload {
+	username: string;
+	following: boolean;
+	slug?: string;
+}
+
+export interface FavoriteArticlePayload extends SlugPayload {
+	favorited: boolean;
+	type?: string;
+}
+
+export interface NewCommentPayload {
+	newComment: string;
+	slug: string;
+}
+
+export interface AddCommentPayload extends SlugPayload, NewCommentPayload {}
+
+export interface DeleteCommentPayload extends SlugPayload {
+	id: number;
+}
+
+export interface FetchFeedPayload {
+	type: string;
+	filter: string;
+	page: number;
+}
+
+export interface SetSessionPayload {
+	session: Session;
+}
+
+export interface ChangeRoutePayload {
+	outlet: string;
+	context: OutletContext;
 }
